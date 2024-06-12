f458036a461d1f44430e695e474b6200e6bfc4da
Sansyou47 <yomesaka.ikkousen@gmail.com>
2024-06-12 13:36:30 +0900

    コメントを追加

--- python/app/function/gemini_demo.py
+++ python/app/function/gemini_demo.py
@@ -9,24 +9,35 @@ app = Blueprint("gemini_demo", __name__)
 
 API_KEY = os.getenv('gemini_api_key')
 
+# テキストのみで会話をする場合の処理
 @app.route('/gemini')
 def gemini():
+    # APIキーを設定
     genai.configure(api_key=API_KEY)
+    # モデルの設定(テキストの場合はgemini-proを使用)
     model = genai.GenerativeModel('gemini-pro')
     
+    # 質問文を入力
     response = model.generate_content("3+3の答えを10文字以内で答えてください。")
     return response.text
 
+# 画像から何かしらの質問をする場合の処理
 @app.route('/gemini/image')
 def gemini_image():
+    # APIキーを設定
     genai.configure(api_key=API_KEY)
 
+    # モデルの設定(画像の場合はgemini-pro-visionを使用)
     model = genai.GenerativeModel('gemini-pro-vision')
 
+    # 画像を読み込む
     picture = [{
+        # 画像のMIMEタイプ
         'mime_type': 'image/jpeg',
+        # 画像をファイルパス(app.pyからの相対パス)から取得し、バイナリデータにする
         'data': Path('function/images/image.jpg').read_bytes()
     }]
+    # 画像に関する質問を入力
     prompt = "この画像には何が写っていますか？:"
 
     response = model.generate_content(
