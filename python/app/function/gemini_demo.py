fe42a12d288e0e25745903e7962b553aaf656683
tsu7321 <kd1303845@st.kobedenshi.ac.jp>
2024-06-18 11:51:12 +0900

    結果後のプレビュー

--- python/app/function/gemini_demo.py
+++ python/app/function/gemini_demo.py
@@ -1,4 +1,5 @@
 import os
+import base64
 import requests
 from pathlib import Path
 from flask import Blueprint, request, render_template, redirect, url_for
@@ -10,20 +11,20 @@ app = Blueprint("gemini_demo", __name__)
 API_KEY = os.getenv('gemini_api_key')
 
 # テキストのみで会話をする場合の処理
-@app.route('/gemini', methods=['GET', 'POST'])
-def gemini():
-    if request.method == 'POST':
-        prompt = request.form['question']
-        # APIキーを設定
-        genai.configure(api_key=API_KEY)
-        # モデルの設定(テキストの場合はgemini-proを使用)
-        model = genai.GenerativeModel('gemini-pro')
+# @app.route('/gemini', methods=['GET', 'POST'])
+# def gemini():
+#     if request.method == 'POST':
+#         prompt = request.form['question']
+#         # APIキーを設定
+#         genai.configure(api_key=API_KEY)
+#         # モデルの設定(テキストの場合はgemini-proを使用)
+#         model = genai.GenerativeModel('gemini-pro')
 
-        # 質問文を入力
-        response = model.generate_content(prompt)
-        return response.text + '<br><a href="/gemini">もう一度質問する</a>'
-    else:
-        return render_template('gemini.html')
+#         # 質問文を入力
+#         response = model.generate_content(prompt)
+#         return response.text + '<br><a href="/gemini">もう一度質問する</a>'
+#     else:
+#         return render_template('gemini.html')
 
 # 画像から何かしらの質問をする場合の処理
 @app.route('/gemini/image' , methods=['GET', 'POST'])
@@ -37,18 +38,24 @@ def gemini_image():
 
         # モデルの設定(画像の場合はgemini-pro-visionを使用)
         model = genai.GenerativeModel('gemini-pro-vision')
-
+        
         # 画像を読み込む
+        picture_data = image.read()
         picture = [{
             # 画像のMIMEタイプ
             'mime_type': 'image/jpeg',
             # 画像をファイルパス(app.pyからの相対パス)から取得し、バイナリデータにする
-            'data': image.read()
+            'data': picture_data
         }]
 
         response = model.generate_content(
             contents=[prompt, picture[0]]
         )
-        return response.text + '<br><a href="/gemini/image">もう一度質問する</a>'
+
+        # 画像をbase64にエンコード
+        encoded_image = base64.b64encode(picture_data).decode('utf-8')
+        # 画像をdataURIに変換
+        data_uri = f"data:image/jpeg;base64,{encoded_image}"
+        return render_template('image_result.html', response=response.text, image=data_uri)        
     else:
         return render_template('image.html')
