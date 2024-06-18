a55b61ae18399b809e4adab346015427841e1f5f
Sansyou47 <yomesaka.ikkousen@gmail.com>
2024-06-18 14:53:14 +0900

    cowsay！！！

--- python/app/app.py
+++ python/app/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 # from PIL import Image
 # 分割したファイルを読み込む。ディレクトリはfunctionディレクトリからの相対パスを指定している。
 from function import pil_demo, variable, blueprint_demo, gemini_demo
+import subprocess, re
 
 app = Flask(__name__)
 
@@ -22,6 +23,17 @@ def pil():
 @app.route('/developers')
 def developers():
     return render_template('developers.html')
+
+@app.route('/contact')
+def contact():
+    # cowsayコマンドを実行
+    result = subprocess.run(['/usr/games/cowsay', '-f', 'dragon', 'コンタクトは取れないヨ！'], encoding='utf-8', stdout=subprocess.PIPE)
+    # 連続する空白を&nbsp;に置換
+    formatted_text = re.sub(r' {2,}', lambda match: '&nbsp;' * len(match.group()), result.stdout)
+    # 改行を<br>タグに置換
+    formatted_text = formatted_text.replace('\n', '<br>')
+    # 置換したテキストをHTMLに渡す
+    return render_template('out.html', text=formatted_text)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
