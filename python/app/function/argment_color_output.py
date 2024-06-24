fa1e9fe73aedaec4dbe37f25121112a01503b127
Sansyou47 <yomesaka.ikkousen@gmail.com>
2024-06-24 13:38:41 +0900

    リストを変更

--- python/app/function/argment_color_output.py
+++ python/app/function/argment_color_output.py
@@ -42,7 +42,7 @@ def write_colors_to_csv(color_codes_with_ratios, csv_path=variable.csv_path):
             writer.writerow([hex_color, ratio])
         
 # 画像からドミナントカラーを抽出する関数
-def extract_dominant_colors(image, num_colors=10):
+def extract_dominant_colors(image, num_colors=15):
     image = Image.open(image)
     pixels = np.array(image).reshape(-1, 3)
     
@@ -66,10 +66,10 @@ def extract_dominant_colors(image, num_colors=10):
     return [(tuple(color), ratio) for color, ratio in zip(dominant_colors, color_ratios)]
 
 # 12色相環を定義
-color_wheel_12 = ['red', 'red-orange', 'yellow-orange',
-               'yellow', 'yellow-green', 'green',
-               'blue-green', 'blue', 'blue-violet',
-               'violet', 'red-violet', 'red']
+color_wheel_12 = ['red', 'orange', 'yellow',
+               'yellow-green', 'green', 'light-green',
+               'green-blue', 'light-blue', 'blue',
+               'purple', 'pink', 'red']
 
 # 24色相環を定義
 color_wheel_24 = ['red', 'vermilion', 'orange', 'amber', 'yellow', 'yellow-green',
@@ -91,19 +91,21 @@ def find_closest_color(hsv_color):
     # HSV：Hue（色相）、Saturation（彩度）、Value（明度）
     hue, saturation, value = hsv_color
     hue *= 360  # 色相を度に変換
-    # 白の閾値（明度がこの値より大きい場合は白と判定）
-    white_threshold = 0.85
+    # 白の閾値（色相の範囲、彩度、明度を指定）
+    white_hue_range = (28, 72)
+    white_saturation_threshold = 0.15
+    white_value_threshold = 0.85
     # 灰色の閾値（彩度がこの値より小さい場合は灰色と判定）
     gray_saturation_threshold = 0.2
     # 黒の閾値（明度がこの値より小さい場合は黒と判定）
-    black_threshold = 0.3
+    black_threshold = 0.2
     # 茶色の判定基準
     brown_hue_range = (0, 40)
     brown_saturation_threshold = 0.3
     brown_value_threshold = 0.2
 
     # 白の判定
-    if value > white_threshold and saturation < 0.1:
+    if  white_hue_range[0] >= hue <= white_hue_range[1] and value > white_value_threshold and saturation < white_saturation_threshold:
         return 'white'
     # 黒の判定
     elif value < black_threshold:
@@ -112,7 +114,7 @@ def find_closest_color(hsv_color):
     elif saturation < gray_saturation_threshold:
         return 'gray'
     # 茶色の判定
-    elif brown_hue_range[0] <= hue <= brown_hue_range[1] and saturation > brown_saturation_threshold and value > brown_value_threshold or brown_hue_range[0] <= hue <= brown_hue_range[1]:
+    elif brown_hue_range[0] <= hue <= brown_hue_range[1] and saturation > brown_saturation_threshold and value > brown_value_threshold: #or brown_hue_range[0] <= hue <= brown_hue_range[1]:
         return 'brown'
     else:
         # 12色相環の判定（30=360/12）
