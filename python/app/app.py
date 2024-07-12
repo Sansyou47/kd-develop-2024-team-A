1cbd1ad056120fb58521aabe52ea665c1803dfdb
Sansyou47 <yomesaka.ikkousen@gmail.com>
2024-07-12 10:46:36 +0900

    最適化

--- python/app/app.py
+++ python/app/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 # from PIL import Image
 # 分割したファイルを読み込む。ディレクトリはfunctionディレクトリからの相対パスを指定している。
-from function import blueprint_demo, gemini_demo, easter_egg, judgment_color, variable, Shortage, remove_background,debug
+from function import blueprint_demo, gemini_demo, easter_egg, judgment_color, Shortage, remove_background, debug
 
 app = Flask(__name__)
 
