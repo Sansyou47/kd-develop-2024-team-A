f0955120e5997e09fedec97d26fb2b0238d59a1b
Sansyou47 <yomesaka.ikkousen@gmail.com>
2024-09-10 10:33:49 +0900

    スペルミスを修正

--- python/app/function/api.py
+++ python/app/function/api.py
@@ -2,6 +2,6 @@ from flask import Blueprint
 
 app = Blueprint("api", __name__)
 
-app.route('/developers/api', methods=['GET', 'POST'])
+app.route('/develop/api', methods=['GET', 'POST'])
 def api():
     return "API"
