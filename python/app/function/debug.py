1ab9284018eafa3f6b6e51179f6541d6c5d31cc8
Sansyou47 <yomesaka.ikkousen@gmail.com>
2024-10-09 11:11:45 +0900

    デバッグファイルの修正

--- python/app/function/debug.py
+++ python/app/function/debug.py
@@ -1,6 +1,5 @@
 # デバック用いろいろ
-
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from function import mysql
 
 # Blueprintの登録（名前はファイル名が定例）
@@ -12,3 +11,14 @@ def blueprint():
     mysql.cur.execute(sql)
     result = mysql.cur.fetchall()
     return str(result)
+
+@app.route('/debug/mysql' , methods=['GET', 'POST'])
+def debug_mysql():
+    if request.method == 'POST':
+        name = request.form.get('name') 
+        value = request.form.get('value')
+        mysql.cur.execute('INSERT INTO test (name, value) VALUES (%s, %s)', (name, value))
+        mysql.conn.commit()
+        return "ok"
+    else:
+        return render_template('debug_mysql.html')
