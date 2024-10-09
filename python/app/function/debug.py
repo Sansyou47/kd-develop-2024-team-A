a80b011ff5514bad8f515e8dd0801764db366ec0
Sansyou47 <yomesaka.ikkousen@gmail.com>
2024-10-09 11:54:11 +0900

    おぼぼぼぼぼ

--- python/app/function/debug.py
+++ python/app/function/debug.py
@@ -8,17 +8,23 @@ app = Blueprint("debug", __name__)
 @app.route('/debug')
 def blueprint():
     sql = 'SELECT * FROM test'
-    mysql.cur.execute(sql)
-    result = mysql.cur.fetchall()
+    try:
+        mysql.cur.execute(sql)
+        result = mysql.cur.fetchall()
+    except Exception as e:
+        return str(e)
     return str(result)
 
 @app.route('/debug/mysql' , methods=['GET', 'POST'])
 def debug_mysql():
     if request.method == 'POST':
-        name = request.form.get('name') 
+        name = request.form.get('name')
         value = request.form.get('value')
-        mysql.cur.execute('INSERT INTO test (name, value) VALUES (%s, %s)', (name, value))
-        mysql.conn.commit()
+        try:
+            mysql.cur.execute('INSERT INTO test (name, value) VALUES (%s, %s)', (name, value))
+            mysql.conn.commit()
+        except Exception as e:
+            return str(e)
         return "ok"
     else:
         return render_template('debug_mysql.html')
