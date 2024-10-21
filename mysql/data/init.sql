f0a52a738c6f71d784248d77f2d4a70327578942
Sansyou47 <yomesaka.ikkousen@gmail.com>
2024-10-21 13:36:36 +0900

    ？？？

--- mysql/data/init.sql
+++ mysql/data/init.sql
@@ -204,7 +204,7 @@ CREATE TABLE users (
     name VARCHAR(255) NOT NULL,
     password VARCHAR(1024) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
-    created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 INSERT INTO users (name, password, email) VALUES 
