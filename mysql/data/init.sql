f15932e959a3332ef28cf3528b037b31595234c3
HASHIMOTO Syunpei <saltfish2229aks@gmail.com>
2024-12-04 11:03:14 +0900

    更新(あえいうえおあお)

--- mysql/data/init.sql
+++ mysql/data/init.sql
@@ -235,6 +235,8 @@ CREATE TABLE users (
     name VARCHAR(255) NOT NULL,
     password VARCHAR(1024) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
+    secret_question VARCHAR(255) NOT NULL,
+    secret_answer VARCHAR(255) NOT NULL,
     create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
