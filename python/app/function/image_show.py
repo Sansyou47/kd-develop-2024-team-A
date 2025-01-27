42b36a7175211c50ea3afc0febd2182b833543f8
Sansyou47 <yomesaka.ikkousen@gmail.com>
2025-01-27 11:28:00 +0900

    ok

--- python/app/function/image_show.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint,render_template
-
-# Blueprintの登録（名前はファイル名が定例）
-app = Blueprint("image_show", __name__)
-
-@app.route('/SnapScoreLogo')
-def blueprint():
-    return render_template('SnapScoreLogo.html')
--- python/tailwind-config.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# tailwind.config.js ファイルのパス
-FILE_PATH="tailwind.config.js"
-
-# content: [], を content: ["/app/templates/**/*.{html,js}"], に書き換える
-sed -i 's/content: \[\],/content: ["\/app\/templates\/\*\*\/\*.{html,js}"],/' "$FILE_PATH"
